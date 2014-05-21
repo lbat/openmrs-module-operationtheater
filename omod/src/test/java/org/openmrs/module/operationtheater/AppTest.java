9e31600b58e82dfb2ee1aabb8a8101e0cb74864c
Lukas Breitwieser <lukas.breitwieser@gmail.com>
2014-05-21 08:47:27 +0200

    created find surgery page based on coreapps.findPatient - adaption not yet finished: TODO webservice for surgery records; still searches for patients instead of surgeries; change some ui labels

--- omod/src/test/java/org/openmrs/module/operationtheater/AppTest.java
+++ omod/src/test/java/org/openmrs/module/operationtheater/AppTest.java
@@ -3,6 +3,7 @@ package org.openmrs.module.operationtheater;
 import org.junit.Test;
 import org.openmrs.module.appframework.AppTestUtil;
 import org.openmrs.module.appframework.domain.AppDescriptor;
+import org.openmrs.module.appframework.domain.AppTemplate;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
@@ -14,10 +15,20 @@ import static org.junit.Assert.assertThat;
 public class AppTest {
 
 	@Test
+	public void testOpenSurgeryAppTemplateIsLoaded() throws Exception {
+		AppTemplate template = AppTestUtil.getAppTemplate("operationtheater.template.openSurgery");
+		assertThat(template.getConfigOptions().get(0).getName(), is("afterSelectedUrl"));
+	}
+
+	@Test
 	public void testOpenSurgeryAppIsLoaded() throws Exception {
 		AppDescriptor app = AppTestUtil.getAppDescriptor("operationtheater.openSurgery");
 		assertThat(app.getOrder(), is(2));
 		assertThat(app.getExtensions(), hasSize(1));
 		assertThat(app.getExtensions().get(0).getExtensionPointId(), is("org.openmrs.referenceapplication.homepageLink"));
+		assertThat(app.getInstanceOf(), is("operationtheater.template.openSurgery"));
+		assertThat(app.getTemplate().getId(), is("operationtheater.template.openSurgery"));
+		String expectedUrl = app.getTemplate().getConfigOptions().get(0).getDefaultValue().getTextValue();
+		assertThat(app.getConfig().get("afterSelectedUrl").getTextValue(), is(expectedUrl));
 	}
 }
