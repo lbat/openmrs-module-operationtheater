97b4432b9e85f9aecf6e630b534b054ce308b403
Lukas Breitwieser <lukas.breitwieser@gmail.com>
2014-06-17 15:00:52 +0200

    setup the environment for lpsolve - demo app running

--- api/src/main/java/org/openmrs/module/operationtheater/OperationTheaterModuleActivator.java
+++ api/src/main/java/org/openmrs/module/operationtheater/OperationTheaterModuleActivator.java
@@ -13,9 +13,17 @@
  */
 package org.openmrs.module.operationtheater;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.Module;
 import org.openmrs.module.ModuleActivator;
+import org.openmrs.module.ModuleClassLoader;
+import org.openmrs.module.ModuleException;
+import org.openmrs.module.ModuleFactory;
+import org.openmrs.module.operationtheater.schedule.Scheduler;
+
+import java.io.File;
 
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
@@ -49,7 +57,58 @@ public class OperationTheaterModuleActivator implements ModuleActivator {
 	 * @see ModuleActivator#started()
 	 */
 	public void started() {
+		copyNativeLibraries();
+		Scheduler.demo();
 		log.info("Operation Theater Module started");
+	}
+
+	/**
+	 * tries to copy the lp_solve native libraries into a directory specified within the java.library.path
+	 */
+	private void copyNativeLibraries() {
+		String libraryPath = System.getProperty("java.library.path");
+
+		//FIXME implement better solution like specifiying the directory as module property instead of trying each of them
+		Module module = ModuleFactory.getModuleById("operationtheater");
+		boolean success = false;
+		for (String path : libraryPath.split(File.pathSeparator)) {
+			try {
+				String modulePath = ModuleClassLoader.getLibCacheFolderForModule(module).getCanonicalPath() + File.separator;
+				System.out.println(modulePath);
+
+				//TODO detect platform computing platform (32bit vs 64bit version)
+				File src = new File(modulePath + "lib" + File.separator + "liblpsolve-5.5-linux-x86_64.so");
+				String libLpsolve = path + File.separator + "liblpsolve55.so";
+				File dest = new File(libLpsolve);
+
+				if (dest.exists()) {
+					FileUtils.forceDelete(dest);
+				}
+
+				FileUtils.copyFile(src, dest);
+
+				src = new File(modulePath + "lib" + File.separator + "liblpsolvej-5.5-linux-x86_64.so");
+				String libJavaLpSolve = path + File.separator + "liblpsolve55j.so";
+				dest = new File(libJavaLpSolve);
+
+				if (dest.exists()) {
+					FileUtils.forceDelete(dest);
+				}
+
+				FileUtils.copyFile(src, dest);
+
+				success = true;
+				break;
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (!success) {
+			log.error("Could not copy native libraries into a directory within the following library path : " + libraryPath);
+			throw new ModuleException(
+					"Could not copy native libraries into a directory within the following library path : " + libraryPath);
+		}
 	}
 
 	/**
