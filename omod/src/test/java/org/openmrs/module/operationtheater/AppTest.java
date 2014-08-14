44f947f9a2a693afee2ff6271766410930c72611
Lukas Breitwieser <lukas.breitwieser@gmail.com>
2014-08-14 15:01:56 +0200

    removed unused code

--- omod/src/test/java/org/openmrs/module/operationtheater/AppTest.java
+++ omod/src/test/java/org/openmrs/module/operationtheater/AppTest.java
@@ -15,34 +15,6 @@ import static org.junit.Assert.assertThat;
 public class AppTest {
 
 	@Test
-	public void testOpenSurgeryAppTemplateIsLoaded() throws Exception {
-		AppTemplate template = AppTestUtil.getAppTemplate("operationtheater.template.openSurgery");
-		assertThat(template.getConfigOptions().get(0).getName(), is("afterSelectedUrl"));
-	}
-
-	@Test
-	public void testOpenSurgeryAppIsLoaded() throws Exception {
-		AppDescriptor app = AppTestUtil.getAppDescriptor("operationtheater.openSurgery");
-		assertThat(app.getOrder(), is(2));
-		assertThat(app.getExtensions(), hasSize(1));
-		assertThat(app.getExtensions().get(0).getExtensionPointId(), is("org.openmrs.referenceapplication.homepageLink"));
-		assertThat(app.getExtensions().get(0).getId(), is("coreapps.activeVisitsHomepageLink"));
-		assertThat(app.getExtensions().get(0).getExtensionPointId(), is("org.openmrs.referenceapplication.homepageLink"));
-		assertThat(app.getExtensions().get(0).getType(), is("link"));
-		assertThat(app.getExtensions().get(0).getLabel(), is("operationtheater.openSurgery.app.button.label"));
-		assertThat(app.getExtensions().get(0).getUrl(),
-				is("operationtheater/findSurgery.page?app=operationtheater.openSurgery"));
-		assertThat(app.getExtensions().get(0).getIcon(), is("icon-file"));
-		//TODO priviledge
-		//assertThat(app.getExtensions().get(0).getRequiredPrivilege(), is());
-
-		assertThat(app.getInstanceOf(), is("operationtheater.template.openSurgery"));
-		assertThat(app.getTemplate().getId(), is("operationtheater.template.openSurgery"));
-		String expectedUrl = app.getTemplate().getConfigOptions().get(0).getDefaultValue().getTextValue();
-		assertThat(app.getConfig().get("afterSelectedUrl").getTextValue(), is(expectedUrl));
-	}
-
-	@Test
 	public void testSchedulingAppIsLoaded() throws Exception {
 		AppDescriptor app = AppTestUtil.getAppDescriptor("operationtheater.scheduling");
 		assertThat(app.getOrder(), is(2));
